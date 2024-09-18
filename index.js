a30c3a95a5d29056783d6618275b982ddc2823aa
Ged45 <125463997+Ged45@users.noreply.github.com>
2024-09-18 17:21:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//mongoose.connect('mongodb://localhost:27017/registration', { useNewUrlParser: true, useUnifiedTopology: true });
-.then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+
+mongoose.connect('mongodb://localhost:27017/registration');
+
+ 
 
 const eventRoutes = require('./routes/event');
 app.use('/api', eventRoutes);
